9ed4b0a3dc25b10d64d9b56d8708f636a67beb35
eemilyy <emilyalmeeidas@gmail.com>
2021-10-27 22:42:57 -0300

    Interacting with the input of the google page

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox()
+
 browser.get('http://google.com')
+browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("Hello")
